5a8b2e8d4f1f5965df3aa8133a664e6f860eaf88
Garigipati Krishna <srinimagi99@gmail.com>
2026-09-07 10:16:22 +0530

    Remove old project files

--- config_core.py
+++
@@ -1,1 +0,0 @@
-from resonance.config_loader import CoreConfig
--- glmx_types.py
+++
@@ -1,7 +0,0 @@
-from resonance.types import Node, Edge, Subgraph
-from resonance.glmx_types import Plan, WalkResult, Answer
-
-__all__ = [
-    "Node", "Edge", "Subgraph",
-    "Plan", "WalkResult", "Answer",
-]
